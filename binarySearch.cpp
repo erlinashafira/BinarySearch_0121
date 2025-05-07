f3a7817e9bfb467eade9fa5437050c3977003044
errerew <erlina31acc@gmail.com>
2025-05-07 12:28:39 +0700

    menambahkan fungsi input

---
+++ binarySearch.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int element[10];
+int nPanjang;
+int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya element pada array"
+    }
+}
